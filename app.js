f3bd335882f0fb41da8ed6f12b44d5d4fb6f3a41
agreat10 <komolov10@gmail.com>
2024-02-11 00:27:36 +0300

    Установили админку

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ let mysql = require('mysql');
 */
 app.use(express.json());
 
+app.use(express.urlencoded());
+
 const nodemailer = require('nodemailer');
 
 let con = mysql.createPool({
@@ -25,6 +27,8 @@ let con = mysql.createPool({
   password: '',
   database: 'market'
 });
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 app.listen(3000, function () {
   console.log('node express work on 3000');
@@ -138,6 +142,7 @@ app.post('/finish-order', function (req, res) {
         if (error) throw error;
         //console.log(result);
         sendMail(req.body, result).catch(console.error);
+        saveOrder(req.body, result);
         res.send('1');
       });
   }
@@ -145,6 +150,101 @@ app.post('/finish-order', function (req, res) {
     res.send('0');
   }
 });
+
+app.get('/admin', function (req, res) {
+  res.render('admin', {});
+});
+
+app.get('/admin-order', function (req, res) {
+  con.query(`SELECT 
+	shop_order.id as id,
+	shop_order.user_id as user_id,
+    shop_order.goods_id as goods_id,
+    shop_order.goods_cost as goods_cost,
+    shop_order.goods_amount as goods_amount,
+    shop_order.total as total,
+    from_unixtime(date,"%Y-%m-%d %h:%m") as human_date,
+    user_info.user_name as user,
+    user_info.user_phone as phone,
+    user_info.address as address
+FROM 
+	shop_order
+LEFT JOIN	
+	user_info
+ON shop_order.user_id = user_info.id ORDER BY id DESC`, function (error, result, fields) {
+      if (error) throw error;
+      console.log(result);
+      res.render('admin-order', { order: JSON.parse(JSON.stringify(result)) });
+    });
+});
+
+/**
+ *  login form ==============================
+ */
+app.get('/login', function (req, res) {
+  res.render('login', {});
+});
+
+app.post('/login', function (req, res) {
+  console.log('!=======================');
+  console.log(req.body);
+  //console.log(req.body.login);
+  //console.log(req.body.password);
+  console.log('=======================!');
+  con.query(
+    'SELECT * FROM user WHERE login="' + req.body.login + '" and password="' + req.body.password + '"',
+    function (error, result) {
+      if (error) reject(error);
+      console.log('result');      
+      console.log(result);
+      console.log('result.length');
+      console.log(result.length);
+      if (result.length == 0) {
+        console.log('error user not found');
+        res.redirect('/login');
+      }
+      else {
+        result = JSON.parse(JSON.stringify(result));
+        //console.log(result[0]['id']);
+       res.cookie('hash', 'blablabla');
+       //res.end('BLASSS');
+        /* *
+         * write hash to db
+         */
+        sql = "UPDATE user  SET hash='blablabla' WHERE id=" + result[0]['id'];
+        con.query(sql, function (error, resultQuery) {
+          if (error) throw error;
+          res.redirect('/admin');
+        }); /**/
+        //console.log('Получилось');
+
+      };
+    });
+});
+
+
+function saveOrder(data, result) {
+  // data - информация о пользователе
+  // result - сведения о товаре
+  let sql;
+  sql = "INSERT INTO user_info (user_name, user_phone, user_email, address) VALUES ('" + data.username + "','" + data.phone + "','" + data.email + "','" + data.address + "')";
+  con.query(sql, function (error, resultQuery) {
+    if (error) throw error;
+    console.log('1 user info saved');
+    console.log(resultQuery);
+    let userId = resultQuery.insertId;
+    date = new Date() / 1000;
+    for (let i = 0; i < result.length; i++) {
+      sql = "INSERT INTO shop_order(date, user_id, goods_id,goods_cost, goods_amount, total) VALUES (" + date + "," + userId + "," + result[i]['id'] + "," + result[i]['cost'] + "," + data.key[result[i]['id']] + "," + data.key[result[i]['id']] * result[i]['cost'] + ")";
+      con.query(sql, function (error, resultQuery) {
+        if (error) throw error;
+        console.log("1 goods saved");
+      })
+    }
+  });
+
+}
+
 
 async function sendMail(data, result) {
   let res = '<h2>Заказ из магазина</h2>';
